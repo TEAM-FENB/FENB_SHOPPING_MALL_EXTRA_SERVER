1b50e11c2f30387a17bc147a88fa03c735c449cb
LEE DONG GYU <tenenger7125@gmail.com>
2023-07-09 11:29:23 +0900

    config: add credentail config

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: '*', // 출처 허용 옵션
+    credential: 'true', // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+  })
+);
 
 app.use('/api', api);
 
