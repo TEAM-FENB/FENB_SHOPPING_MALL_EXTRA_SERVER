4b429baa59b4e180cf1e52a18878a74b45f9a8bb
LEE DONG GYU <tenenger7125@gmail.com>
2023-08-01 21:55:44 +0900

    fix: fix cors error

--- app.js
+++ app.js
@@ -8,17 +8,20 @@ require('dotenv').config();
 const app = express();
 const api = require('./routes/api');
 const PORT = process.env.PORT || 8000;
+const whiteList = ['http://localhost:5173', 'https://486-shoe.shop'];
+const corsOptions = {
+  origin(origin, callback) {
+    const isInWhiteList = whiteList.indexOf(origin) !== -1; // 만일 whitelist 배열에 origin인자가 있을 경우
+    callback(null, isInWhiteList); // cors 허용 / 거부
+  },
+  credential: true,
+};
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: ['http://localhost:5173', 'https://486-shoe.shop'],
-    credential: true,
-  })
-);
+app.use(cors(corsOptions));
 
 app.use('/api', api);
 
