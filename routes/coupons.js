ffd0280c601036ee43148364a1ca6c9d84a90ad3
LEE DONG GYU <tenenger7125@gmail.com>
2023-08-20 18:01:33 +0900

    fix: update to get coupon for new Account

--- routes/coupons.js
+++ routes/coupons.js
@@ -22,11 +22,11 @@ router.post('/:id', authCheck, expireCoupon, async (req, res) => {
   const coupon = await getCoupon(couponId);
   if (!coupon) return res.status(404).send({ message: '요청하신 쿠폰이 없습니다.' });
 
-  const user = getUser(email);
+  const user = await getUser(email);
 
   if (
     '64a051f8a1df112941c05596' === couponId &&
-    getDateAfter(coupon.validDate, user.createdAt) < new Date().getTime()
+    getDateAfter(coupon.validDate, user.createdAt).getTime() < new Date().getTime()
   ) {
     // 신규가입 쿠폰을 발급
     return res.status(401).send({ message: '쿠폰 받는 유효기간이 지났습니다.' });
